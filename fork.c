704ef67babe88c64abbaaea92f345a24a408e97a
guoyao <guoyao.dev@gmail.com>
2014-09-02 23:23:39 -0400

    format code

--- fork.c
+++ fork.c
@@ -4,24 +4,24 @@
 
 main(int argc, char* argv[]) {
     int pid;
-	printf("Now only one process\n");
-  	printf("Calling fork… \n");
-  	pid = fork();
-	if (!pid) {
-		char* arguments[argc - 1];
-		int i = 0;
-		while(++i < argc) {
-			arguments[i - 1] = argv[i];	
-		}
-		arguments[argc - 1] = NULL;
+    printf("Now only one process\n");
+    printf("Calling fork… \n");
+    pid = fork();
+    if (!pid) {
+        char* arguments[argc - 1];
+        int i = 0;
+        while(++i < argc) {
+            arguments[i - 1] = argv[i]; 
+        }
+        arguments[argc - 1] = NULL;
         execvp(argv[1], arguments);
         exit(0);
-	} else if (pid > 0) {
+    } else if (pid > 0) {
         wait(NULL);
-		printf("I’m the parent, child has pid %d\n", pid);
-		exit(0);
-	} else {
-		printf("Fork fail!\n");
-		exit(1);
-	}
+        printf("I’m the parent, child has pid %d\n", pid);
+        exit(0);
+    } else {
+        printf("Fork fail!\n");
+        exit(1);
+    }
 }
